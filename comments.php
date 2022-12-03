dd6e2ddc137f0a7c603f8b835d5b283fa5ed0015
Azeddine Hf <azeddine.ha15@gmail.com>
2022-12-03 22:39:52 +0100

    new modif

--- comments.php
+++ comments.php
@@ -1,130 +1,15 @@
+<?php include_once 'dbconfig.class.php' ?>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="utf-8">
-    <!--  This file has been downloaded from bootdey.com @bootdey on twitter -->
-    <!--  All snippets are MIT license http://bootdey.com/license -->
-    <title>blog item comments - Bootdey.com</title>
+    <title>Les Avis</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
-    <link href="https://netdna.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
-    <script src="https://netdna.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-</head>
-
-<body>
-    <section class="content-item" id="comments">
-                <div class="d-flex justify-content-center align-items-center container">
-                    <div class="">
-                    <form class="form">
-                        <h3 class="pull-left">New Comment</h3>
-                        <fieldset>
-                            <div class="">
-                                <div class="col-sm-3 col-lg-2 hidden-xs">
-                                    <img class="img-responsive" src="uploads/user.png" alt="">
-                                </div>
-                                    <div class="col-xs-12 col-sm-10">
-                                        <div class="form-group fl_icon">
-                                            <div class="icon"><i class="fa fa-user"></i></div>
-                                            <input class="form-control" type="text" placeholder="Your name">
-                                        </div>
-                                    </div>
-                                    <div class="col-xs-12 col-sm-10 fl_icon">
-                                        <div class="form-group fl_icon">
-                                            <div class="icon"><i class="fa fa-envelope-o"></i></div>
-                                            <input class="form-control" type="text" placeholder="Your email">
-                                        </div>
-                                    </div>
-                                    <div class="col-xs-12">
-                                        <div class="form-group">
-                                            <textarea class="form-control" required="" placeholder="Your text"></textarea>
-                                        </div>
-                                    </div>
-                                    <a class="btn btn-primary pull-right">submit</a>
-                                </div>
-                        </fieldset>
-                    </form>
-
-                    <h3>4 Comments</h3>
-
-                    <!-- COMMENT 1 - START -->
-                    <div class="media">
-                        <a class="pull-left" href="#"><img class="media-object" src="https://bootdey.com/img/Content/avatar/avatar1.png" alt=""></a>
-                        <div class="media-body">
-                            <h4 class="media-heading">John Doe</h4>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                            <ul class="list-unstyled list-inline media-detail pull-left">
-                                <li><i class="fa fa-calendar"></i>27/02/2014</li>
-                                <li><i class="fa fa-thumbs-up"></i>13</li>
-                            </ul>
-                            <ul class="list-unstyled list-inline media-detail pull-right">
-                                <li class=""><a href="">Like</a></li>
-                                <li class=""><a href="">Reply</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                    <!-- COMMENT 1 - END -->
-
-                    <!-- COMMENT 2 - START -->
-                    <div class="media">
-                        <a class="pull-left" href="#"><img class="media-object" src="https://bootdey.com/img/Content/avatar/avatar2.png" alt=""></a>
-                        <div class="media-body">
-                            <h4 class="media-heading">John Doe</h4>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                            <ul class="list-unstyled list-inline media-detail pull-left">
-                                <li><i class="fa fa-calendar"></i>27/02/2014</li>
-                                <li><i class="fa fa-thumbs-up"></i>13</li>
-                            </ul>
-                            <ul class="list-unstyled list-inline media-detail pull-right">
-                                <li class=""><a href="">Like</a></li>
-                                <li class=""><a href="">Reply</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                    <!-- COMMENT 2 - END -->
-
-                    <!-- COMMENT 3 - START -->
-                    <div class="media">
-                        <a class="pull-left" href="#"><img class="media-object" src="https://bootdey.com/img/Content/avatar/avatar3.png" alt=""></a>
-                        <div class="media-body">
-                            <h4 class="media-heading">John Doe</h4>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                            <ul class="list-unstyled list-inline media-detail pull-left">
-                                <li><i class="fa fa-calendar"></i>27/02/2014</li>
-                                <li><i class="fa fa-thumbs-up"></i>13</li>
-                            </ul>
-                            <ul class="list-unstyled list-inline media-detail pull-right">
-                                <li class=""><a href="">Like</a></li>
-                                <li class=""><a href="">Reply</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                    <!-- COMMENT 3 - END -->
-
-                    <!-- COMMENT 4 - START -->
-                    <div class="media">
-                        <a class="pull-left" href="#"><img class="media-object" src="https://bootdey.com/img/Content/avatar/avatar4.png" alt=""></a>
-                        <div class="media-body">
-                            <h4 class="media-heading">John Doe</h4>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                            <ul class="list-unstyled list-inline media-detail pull-left">
-                                <li><i class="fa fa-calendar"></i>27/02/2014</li>
-                                <li><i class="fa fa-thumbs-up"></i>13</li>
-                            </ul>
-                            <ul class="list-unstyled list-inline media-detail pull-right">
-                                <li class=""><a href="">Like</a></li>
-                                <li class=""><a href="">Reply</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                    </div>
-                    
-                    <!-- COMMENT 4 - END -->
-
-            </div>
-    </section>
-
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
     <style type="text/css">
         body {
             margin-top: 20px;
@@ -256,6 +141,137 @@
             margin-right: 10px;
         }
     </style>
+
+
+</head>
+
+<body>
+    <section class="content-item" id="comments">
+        <div class="d-flex justify-content-center align-items-center container">
+            <div class="">
+                <?php
+                $database = new Connection();
+                $db = $database->openConnection();
+                $stmt = $db->prepare('select * from images where id=' . $_REQUEST["id"] . '');
+                $stmt->execute();
+                $imagelist = $stmt->fetch();
+                ?>
+                <div class="w-50 ml-0 mr-0 mx-auto">
+                    <img src="<?php echo $imagelist['image'] ?>" class="" alt="" width="400" height="400">
+                    <div class="">
+                        <h5 class="title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+                <form class="form">
+
+                    <h2 class=""><span class="badge rounded-pill bg-primary">Ajouter un nouveau commentaire :</span></h2>
+                    <fieldset>
+                        <div class="row">
+                            <div class="col-sm-2 col-xs-1">
+                                <img class="rounded-pill" src="uploads/user.png" alt="" width="100" height="90">
+                            </div>
+                            <div class="col-sm-5">
+                                <div class="form-group fl_icon">
+                                    <div class="icon"><i class="fa fa-user"></i></div>
+                                    <input class="form-control" type="text" placeholder="Your name">
+                                </div>
+                            </div>
+                            <div class="col-sm-5">
+                                <div class="form-group fl_icon">
+                                    <div class="icon"><i class="fa fa-envelope-o"></i></div>
+                                    <input class="form-control " type="text" placeholder="Your email">
+                                </div>
+                            </div>
+                            <div class="col-sm-12">
+                                <div class="form-group">
+                                    <textarea class="form-control mt-2" required="" placeholder="Your text"></textarea>
+                                </div>
+                            </div class="col-sm-12">
+                            <a class="btn btn-success fw-bold">Envoyer</a>
+                        </div>
+                    </fieldset>
+                </form>
+
+                <h3>4 Comments</h3>
+
+                <!-- COMMENT 1 - START -->
+                <div class="media">
+                    <a class="pull-left" href="#"><img class="media-object" src="https://bootdey.com/img/Content/avatar/avatar1.png" alt=""></a>
+                    <div class="media-body">
+                        <h4 class="media-heading">John Doe</h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        <ul class="list-unstyled list-inline media-detail pull-left">
+                            <li><i class="fa fa-calendar"></i>27/02/2014</li>
+                            <li><i class="fa fa-thumbs-up"></i>13</li>
+                        </ul>
+                        <ul class="list-unstyled list-inline media-detail pull-right">
+                            <li class=""><a href="">Like</a></li>
+                            <li class=""><a href="">Reply</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <!-- COMMENT 1 - END -->
+
+                <!-- COMMENT 2 - START -->
+                <div class="media">
+                    <a class="pull-left" href="#"><img class="media-object" src="https://bootdey.com/img/Content/avatar/avatar2.png" alt=""></a>
+                    <div class="media-body">
+                        <h4 class="media-heading">John Doe</h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        <ul class="list-unstyled list-inline media-detail pull-left">
+                            <li><i class="fa fa-calendar"></i>27/02/2014</li>
+                            <li><i class="fa fa-thumbs-up"></i>13</li>
+                        </ul>
+                        <ul class="list-unstyled list-inline media-detail pull-right">
+                            <li class=""><a href="">Like</a></li>
+                            <li class=""><a href="">Reply</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <!-- COMMENT 2 - END -->
+
+                <!-- COMMENT 3 - START -->
+                <div class="media">
+                    <a class="pull-left" href="#"><img class="media-object" src="https://bootdey.com/img/Content/avatar/avatar3.png" alt=""></a>
+                    <div class="media-body">
+                        <h4 class="media-heading">John Doe</h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        <ul class="list-unstyled list-inline media-detail pull-left">
+                            <li><i class="fa fa-calendar"></i>27/02/2014</li>
+                            <li><i class="fa fa-thumbs-up"></i>13</li>
+                        </ul>
+                        <ul class="list-unstyled list-inline media-detail pull-right">
+                            <li class=""><a href="">Like</a></li>
+                            <li class=""><a href="">Reply</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <!-- COMMENT 3 - END -->
+
+                <!-- COMMENT 4 - START -->
+                <div class="media">
+                    <a class="pull-left" href="#"><img class="media-object" src="https://bootdey.com/img/Content/avatar/avatar4.png" alt=""></a>
+                    <div class="media-body">
+                        <h4 class="media-heading">John Doe</h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        <ul class="list-unstyled list-inline media-detail pull-left">
+                            <li><i class="fa fa-calendar"></i>27/02/2014</li>
+                            <li><i class="fa fa-thumbs-up"></i>13</li>
+                        </ul>
+                        <ul class="list-unstyled list-inline media-detail pull-right">
+                            <li class=""><a href="">Like</a></li>
+                            <li class=""><a href="">Reply</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            <!-- COMMENT 4 - END -->
+
+        </div>
+    </section>
+
 
     <script type="text/javascript">
 
--- view.php
+++ view.php
@@ -10,109 +10,197 @@ include_once 'dbconfig.class.php';
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+	<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
-<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
-<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<!------ Include the above in your HEAD tag ---------->
+	<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<!------ Include the above in your HEAD tag ---------->
 
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/magnific-popup.css" />
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.isotope/3.0.6/isotope.pkgd.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/jquery.magnific-popup.js"></script>
-<style>
-    body{
-	margin:0;
-	padding:0;
-}
-/* .container{
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/magnific-popup.css" />
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.isotope/3.0.6/isotope.pkgd.js"></script>
+	<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/jquery.magnific-popup.js"></script>
+	<style>
+		body {
+			margin: 0;
+			padding: 0;
+		}
+
+		/* .container{
 	width:90%
 	margin:10px auto;
 } */
-.portfolio-menu{
-	text-align:center;
-}
-.portfolio-menu ul li{
-	display:inline-block;
-	margin:0;
-	list-style:none;
-	padding:10px 15px;
-	cursor:pointer;
-	-webkit-transition:all 05s ease;
-	-moz-transition:all 05s ease;
-	-ms-transition:all 05s ease;
-	-o-transition:all 05s ease;
-	transition:all .5s ease;
-}
+		.portfolio-menu {
+			text-align: center;
+		}
+
+		.portfolio-menu ul li {
+			display: inline-block;
+			margin: 0;
+			list-style: none;
+			padding: 10px 15px;
+			cursor: pointer;
+			-webkit-transition: all 05s ease;
+			-moz-transition: all 05s ease;
+			-ms-transition: all 05s ease;
+			-o-transition: all 05s ease;
+			transition: all .5s ease;
+		}
 
 
-.portfolio-item .item{
-	/*width:303px;*/
-	float:left;
-	margin-bottom:10px;
-}
+		.portfolio-item .item {
+			/*width:303px;*/
+			float: left;
+			margin-bottom: 10px;
+		}
 
-</style>
+		.badge.even-larger-badge {
+			font-size: 1.1em;
+		}
+	</style>
 </head>
 
 <body>
-<div class="container">
-         <div class="row">
-            <div class="col-lg-12 text-center my-2">
-                <h4>Isotope filter magical layouts with Bootstrap 4</h4>
-            </div>
-         </div>
-         <div class="portfolio-menu mt-2 mb-4">
-            <ul>
-               <li class="btn btn-outline-dark active" data-filter="*">All</li>
-            </ul>
-         </div>
-         <div class="portfolio-item row">
-	<?php
-	$database = new Connection();
-     $db = $database->openConnection();
-	$stmt = $db->prepare('select * from images');
-	$stmt->execute();
-	$imagelist = $stmt->fetchAll();
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12 my-4">
+				<a href="index" class="btn btn-success"><i class="fa fa-plus"></i> Ajouter une nouvelle image</a>
+			</div>
+			<div class="col-lg-12 text-center my-1">
+				<h1><span class="mt-2 badge rounded-pill badge-primary ">Image Gallerie</span></h1>
+			</div>
+		</div>
+		<!-- <div class="portfolio-menu mt-2 mb-4">
+			<ul>
+				<li class="btn btn-outline-dark active" data-filter="*">All</li>
+			</ul>
+		</div> -->
+		<div class="portfolio-item row mt-2">
+			<?php
+			$database = new Connection();
+			$db = $database->openConnection();
+			$stmt = $db->prepare('select * from images');
+			$stmt->execute();
+			$imagelist = $stmt->fetchAll();
 
-	foreach($imagelist as $image) {
-	?>
-		<div class="item selfie col-lg-3 col-md-4 col-6 col-sm">
-               <a href="<?php echo $image['image']?>" class="fancylight popup-btn" data-fancybox-group="light"> 
-               <img class="img-fluid" src="<?php echo $image['image'] ?>" title="<?=$image['name'] ?>" width='200' height='200'>
-               </a><a class="btn btn-info text-white mt-1">Les avis</a><a class="btn text-danger"><i class="fa fa-trash"></i></a>
-            </div>
-	
-	<?php
-	}
-	?>
- </div>
-</div>
+			foreach ($imagelist as $image) {
+			?>
 
-<script>
-            // $('.portfolio-item').isotope({
-        //  	itemSelector: '.item',
-        //  	layoutMode: 'fitRows'
-        //  });
-        $('.portfolio-menu ul li').click(function(){
-         	$('.portfolio-menu ul li').removeClass('active');
-         	$(this).addClass('active');
-         	
-         	var selector = $(this).attr('data-filter');
-         	$('.portfolio-item').isotope({
-         		filter:selector
-         	});
-         	return  false;
-         });
-         $(document).ready(function() {
-         var popup_btn = $('.popup-btn');
-         popup_btn.magnificPopup({
-         type : 'image',
-         gallery : {
-         	enabled : true
-         }
-         });
-         });
-</script>
+				<div class="item selfie col-lg-3 col-md-4 col-6 col-sm">
+					<a href="<?php echo $image['image'] ?>" class="fancylight popup-btn" data-fancybox-group="light">
+						<img class="img-fluid" src="<?php echo $image['image'] ?>" title="<?= $image['name'] ?>" width='170' height='170'>
+					</a>
+					<div class="w-50 ml-0 mr-0 mx-auto mt-2">
+						<a class="btn btn-warning text-white mt-1" href="comments?id=<?php echo $image['id'] ?>"><i class="fa fa-comments"></i> Avis</a>
+						<a href="#myModal" role="button" class="btn text-danger" data-id="4" data-toggle="modal" data-toggle="modal" data-id_img="<?php echo $image['id'] ?>"><i class="fa fa-trash-o"></i></a>
+					</div>
+				</div>
+
+			<?php
+			}
+			?>
+		</div>
+	</div>
+
+	<!--Modal for delete img-->
+	<div id="myModal" class="modal fade bs-example-modal-sm" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+		<div class="modal-dialog">
+			<div class="modal-content">
+				<div class="modal-header flex-column">
+					<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+					<h4 class="modal-title w-100 text-center text-danger">Supprimer l'image</h4>
+				</div>
+				<div class="modal-body">
+					<form action="">
+						<input type="hidden" name="id_imgsup" id="showimg_id" value="">
+						<h2 class="text-danger text-center">confirmer la supprition ?</h2>
+						<h2 class="text-danger text-center">😕</h2>
+				</div>
+				<div class="modal-footer">
+					<button class="btn" data-dismiss="modal" aria-hidden="true">Close</button>
+					<button class="btn btn-danger" type="submit">supprimer</button>
+					</form>
+
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div id="myModal" class="modal hide">
+		<div class="modal-header">
+			<a href="#" data-dismiss="modal" aria-hidden="true" class="close">×</a>
+			<h3>Delete</h3>
+		</div>
+		<div class="modal-body">
+			<p>You are about to delete.</p>
+			<p>Do you want to proceed?</p>
+		</div>
+		<div class="modal-footer">
+			<a href="#" id="btnYes" class="btn danger">Yes</a>
+			<a href="#" data-dismiss="modal" aria-hidden="true" class="btn secondary">No</a>
+		</div>
+	</div>
+
+
+
+	<!--scriiipts-->
+	<script>
+		$(function() {
+			$('#myModal').on('show.bs.modal', function(event) {
+				var button = $(event.relatedTarget);
+				var id_img = button.data('id_img'); 
+				var modal = $(this);
+				modal.find('#showimg_id').val(id_img);
+			});
+		});
+	</script>
+
+	<script>
+		$('#myModal').on('show', function() {
+			var id = $(this).data('id'),
+				removeBtn = $(this).find('.danger');
+		})
+
+		$('.confirm-delete').on('click', function(e) {
+			e.preventDefault();
+
+			var id = $(this).data('id');
+			$('#myModal').data('id', id).modal('show');
+		});
+
+		$('#btnYes').click(function() {
+			// handle deletion here
+			var id = $('#myModal').data('id');
+			$('[data-id=' + id + ']').remove();
+			$('#myModal').modal('hide');
+		});
+	</script>
+	<script>
+		// $('.portfolio-item').isotope({
+		//  	itemSelector: '.item',
+		//  	layoutMode: 'fitRows'
+		//  });
+		$('.portfolio-menu ul li').click(function() {
+			$('.portfolio-menu ul li').removeClass('active');
+			$(this).addClass('active');
+
+			var selector = $(this).attr('data-filter');
+			$('.portfolio-item').isotope({
+				filter: selector
+			});
+			return false;
+		});
+		$(document).ready(function() {
+			var popup_btn = $('.popup-btn');
+			popup_btn.magnificPopup({
+				type: 'image',
+				gallery: {
+					enabled: true
+				}
+			});
+		});
+	</script>
 </body>
 
 </html>
